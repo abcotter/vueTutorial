138a212585519236fa6b62b2470c27111fc8653b
Abigail Cotter <abigail.cotter@gmail.com>
2020-04-17 10:58:37 -0400

    lesson 5

--- main.js
+++ main.js
@@ -2,21 +2,34 @@ var app = new Vue({
     el: '#app',
     data: {
         Product: 'Socks',
-        image: './assets/vmSocks-green-onWhite.jpg',
         inventory: 0,
+        image: './assets/vmSocks-green-onWhite.jpg',
         onSale: true,
         details: ["80% cotton", "20% polyester", "gender-neutral"],
         variants: [
             {
               variantId: 2234,
-              variantColor: 'green'    
+              variantColor: 'green',
+              variantImage: './assets/vmSocks-green-onWhite.jpg'    
             },
             {
               variantId: 2235,
-              variantColor: 'blue'
+              variantColor: 'blue',
+              variantImage: './assets/vmSocks-blue-onWhite.jpg'
             }
         ],
         sizes: ['S', 'M', 'L', 'XL', 'XXL', 'XXXL'],
         cart: 0
+    },
+    methods: {
+      addToCart() {
+        this.cart += 1
+      },
+      updateImage(currentImage) {
+        this.image = currentImage
+      },
+      removeFromCart() {
+        this.cart -=1
+      }
     }
 })
